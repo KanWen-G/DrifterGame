b9a7bd9c5ba5f08fe3cbcc6c6d83946e9d940305
KanWen <qtang4@ucsc.edu>
2019-08-12 05:19:36 -0700

    add tiled file

---
+++ assets/Tiled/Universal Platforms.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="Player" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="37232.png" width="512" height="512"/>
+</tileset>

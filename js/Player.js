cb90461891e205386f3d0bd9da6d67b09f94817c
Kan <qtang4@ucsc.edu>
2019-08-20 14:34:20 -0700

    Changed the tiled and player prefeb

---
+++ js/Player.js
@@ -0,0 +1,54 @@
+function Player(game,x,y) {
+    //new Sprite(game, x, y, key, frame)
+    Phaser.Sprite.call(this, game,x, y, 'player');
+	// Sprite setting
+    this.anchor.set(0.5, 0.5);
+	//physics
+	game.physics.enable(this);
+    this.body.collideWorldBounds = true;
+    //this.body.setSize();
+    this.maxSpeedNormal = new Phaser.Point(300, 10000);
+    this.body.maxVelocity = this.maxSpeedNormal;
+    this.maxSpeedShift = new Phaser.Point(500, 10000);
+    this.accel = 1500;
+    this.pause = true;
+    this.body.gravity.y = 4000;
+}
+// explicitly define prefab's prototype (Phaser.Sprite) and constructor (Player)
+Player.prototype = Object.create(Phaser.Sprite.prototype);
+Player.prototype.constructor = Player;
+
+// override Phaser.Sprite update (to spin the diamond)
+Player.prototype.update = function () {
+    if (game.input.keyboard.isDown(Phaser.Keyboard.SHIFT)) {
+        this.body.maxVelocity = this.maxSpeedShift;
+    } else {
+        this.body.maxVelocity = this.maxSpeedNormal;
+    }
+    //Movement code
+    var xAcc = 0;
+    var yAcc = 0;
+    if (game.input.keyboard.isDown(Phaser.Keyboard.UP) ||
+        game.input.keyboard.isDown(Phaser.Keyboard.W)){
+        if (this.body.onFloor())
+        {
+            this.body.velocity.y = -700;
+        }
+	}
+    if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT) ||
+        game.input.keyboard.isDown(Phaser.Keyboard.D)){
+        xAcc += this.accel;
+        //this.animations.play('right');
+    }
+    if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT) ||
+        game.input.keyboard.isDown(Phaser.Keyboard.A)){
+        xAcc -= this.accel;
+        //this.animations.play('left');
+    }
+    this.body.acceleration.x = xAcc;
+    this.body.acceleration.y = yAcc;
+    if (xAcc == 0)
+        this.body.velocity.x = 0;
+}
+
+

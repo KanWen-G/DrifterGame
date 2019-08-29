fc29517609de4ce13bf0331197615441b6fa49a8
KanWen-G <53582578+KanWen-G@users.noreply.github.com>
2019-08-29 03:03:54 -0700

    Update unlock.js

--- js/prefabs/unlock.js
+++ js/prefabs/unlock.js
@@ -3,7 +3,6 @@ function unlocking(word){
     if(game.input.keyboard.justPressed(Phaser.Keyboard.ENTER)){
         this.lock = game.add.sprite(game.camera.x + 300, game.camera.y + 150, 'lock');
         this.lock.animations.add('error', [0,1], 14, true, true);
-        game.world.bringToTop(this.lock);
         this.frame = 0;
         isLock = false;
         this.lockedSound = game.add.audio('Locked');
@@ -26,7 +25,7 @@ this.lock.animations.stop()
 }
 
 function killLock(){
-this.lock.alpha = 0;;
+this.lock.kill();
 }
 
 function keyPress (char){ 
